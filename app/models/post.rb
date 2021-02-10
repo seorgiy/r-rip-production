9c2f6fbd61e05257956a917495d3aeef2d85f9b2
Seorgiy <george.sergeev2014@gmail.com>
2021-02-10 23:42:00 +0300

    pagination & approved posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,6 @@ class Post < ApplicationRecord
   belongs_to :user, optional: true, foreign_key: :author
   belongs_to :admin_user, optional: true, foreign_key: :moderator
   validates :text, presence: true
+
+scope :approved, -> { where(approved: true) }
 end
