9e4673e151f25a252a1d8ef5f8cb12586bd21d5c
Seorgiy <george.sergeev2014@gmail.com>
2021-02-15 21:40:49 +0300

    port 3000 fixed

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,8 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  # def page(count)
+  #   p count
+  #   self.offset(20*count).limit(20).order(created_at: :desc)
+  # end
 end
