b63077df7c9e6dec8254b4f813f234547391cacf
Seorgiy <george.sergeev2014@gmail.com>
2021-02-23 23:53:45 +0300

    show action fixed

--- app/controllers/api/v1/artifacts_controller.rb
+++ app/controllers/api/v1/artifacts_controller.rb
@@ -4,28 +4,18 @@ module Api
       def index
         page = params[:page].to_i
         artifacts = Artifact.offset(20*(page)).limit(20).order(created_at: :desc)
-        render jsonapi: artifacts,
-        fields: { artifacts: [:text] }, class: { Artifact: Api::V1::SerializableArtifact }
+        render jsonapi: artifacts, class: { Artifact: Api::V1::SerializableArtifact }
       end
 
-      # def show
-      #     render jsonapi: Artifact.find(params[:id]), class: { Artifact: Api::V1::SerializableArtifact }
-      # end
+      def show
+        render jsonapi: Artifact.find(params[:id]), class: { Artifact: Api::V1::SerializableArtifact }
+      end
 
       def create
         artifact = Artifact.create(artifact_params)
 
-        render jsonapi: artifact,
-        fields: { artifacts: [:title] }, class: { Artifact: Api::V1::SerializableArtifact }
+        render jsonapi: artifact, class: { Artifact: Api::V1::SerializableArtifact }
       end
-
-      # def update
-      #   Artifact = Artifact.find(params[:id])
-      #   Artifact.update!(text: params[:text], signature: params[:signature], author: params[:author])
-
-      #   render jsonapi: Artifact, include: [:author],
-      #   fields: { Artifacts: [:text] }, class: { Artifact: Api::V1::SerializableArtifact }
-      # end
 
       private
 
