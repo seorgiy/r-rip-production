e2fa6d7e6796f8c895859a4e446ac2d98ee23eee
Seorgiy <george.sergeev2014@gmail.com>
2021-03-02 22:10:58 +0300

    post attachments fixed

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,11 +2,15 @@ class Post < ApplicationRecord
   belongs_to :user, optional: true, foreign_key: :author
   belongs_to :admin_user, optional: true, foreign_key: :moderator
   validates :text, presence: true
-  has_many_attached :attachments
+  validate :attachments, :is_json?
 
   scope :approved, -> { where(approved: true) }
 
   def get_author
    self.user.present? ? self.user.email : self.signature
+  end
+
+  def is_json?
+    !!JSON.parse(self.attachments)
   end
 end
