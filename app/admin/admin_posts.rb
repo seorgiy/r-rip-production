2600016dde039010738b512b50b25a044b74017f
Seorgiy <george.sergeev2014@gmail.com>
2021-03-12 19:05:32 +0300

    datepicker in posts admin

--- app/admin/admin_posts.rb
+++ app/admin/admin_posts.rb
@@ -3,6 +3,19 @@ ActiveAdmin.register Post do
   includes :user
   includes :admin_user
 
+  form do |f|
+    f.inputs do
+      f.input :user
+      f.input :text
+      f.input :published_at, as: :datepicker
+      f.input :signature
+      f.input :employee_only
+      f.input :approved
+      f.input :attachments
+    end
+    f.actions
+  end
+
   member_action :update, method: :post do
     new_post = params[:post]
     resource.update!(
@@ -12,7 +25,8 @@ ActiveAdmin.register Post do
       employee_only: new_post[:employee_only] == '1' ? true : false,
       approved: new_post[:approved] == '1' ? true : false,
       moderator: current_admin_user.id,
-      attachments: params[:attachments],
+      attachments: new_post[:attachments],
+      published_at: new_post[:published_at],
       updated_at: Time.now
     )
 
@@ -28,7 +42,8 @@ ActiveAdmin.register Post do
         employee_only: new_post[:employee_only] == '1' ? true : false,
         approved: new_post[:approved] == '1' ? true : false,
         moderator: current_admin_user.id,
-        attachments: params[:attachments],
+        attachments: new_post[:attachments],
+        published_at: new_post[:published_at],
         updated_at: Time.now
     ).save!
 
@@ -59,6 +74,8 @@ ActiveAdmin.register Post do
     actions do |post|
       link_to I18n.t('active_admin.approve'), "#{collection_path}/#{post.id}/approve/"
     end
+
+
 
 
   end
