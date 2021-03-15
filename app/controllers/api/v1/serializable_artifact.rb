95eed5ccf29f6c6990ad81bf715664928f6d1391
Seorgiy <george.sergeev2014@gmail.com>
2021-03-16 00:45:23 +0300

    video preview

--- app/controllers/api/v1/serializable_artifact.rb
+++ app/controllers/api/v1/serializable_artifact.rb
@@ -1,7 +1,11 @@
 class Api::V1::SerializableArtifact < JSONAPI::Serializable::Resource
   type 'artifact'
 
-  attributes :title, :description, :url, :category
+  attributes :title, :description, :url, :category, :preview
+
+  # attribute :preview do
+  #   @object.attachment.preview(resize_to_limit: [800,800]).processed.url if @object.attachment&.blob&.content_type == "video/mp4"
+  # end
 
   attribute :additional do
     JSON.parse(@object.additional) unless @object.additional.nil?
