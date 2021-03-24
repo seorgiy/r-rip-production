d95bf96274f99660ddac6e6ab37b57ff4ccef898
Seorgiy <george.sergeev2014@gmail.com>
2021-03-24 23:01:40 +0300

    clean some empty files

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,8 +1,3 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
-
-  # def page(count)
-  #   p count
-  #   self.offset(20*count).limit(20).order(created_at: :desc)
-  # end
 end
