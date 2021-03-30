1c564dc0c948d74caed23b25b83b8a1ef219128f
Seorgiy <george.sergeev2014@gmail.com>
2021-03-30 23:05:12 +0300

    admin user login telegram

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -8,10 +8,7 @@ class AdminUser < ApplicationRecord
 
   class << self
     def from_omniauth(auth)
-      p auth
-      p 'что-то пытаюсь'
-      admin_user = where(email: auth.info.email).first || where(auth.slice(:provider, :uid).to_h).first || new
-      admin_user.tap { |this| this.update_attributes(provider: auth.provider, uid: auth.uid, email: auth.info.email) }
+      admin_user = where(provider: auth.provider, provider_id: auth.uid).first
     end
   end
 end
