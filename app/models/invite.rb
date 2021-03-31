bfd52c1955338b1621b2afef43539ba129fb7ac0
Seorgiy <george.sergeev2014@gmail.com>
2021-03-31 23:16:34 +0300

    email invites fixed

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -2,7 +2,10 @@ class Invite < ApplicationRecord
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :name, presence: true
   validates :tg, presence: true
-  attr_accessor :email
+
+  def email
+    self[:email]
+  end
 
   before_save do
     self.tg = self.tg.sub("@", "")
